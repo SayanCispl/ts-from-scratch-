3afe44b240ee113ccf2f5ce187a29d9398f5c424
SayanCispl <sayan.koley@codeclouds.co.in>
2026-05-26 20:02:32 +0530

    added js basic concepts

---
+++ JsConcepts/jsconcepts.js
@@ -0,0 +1,13 @@
+let a1 = 10
+let b1 = 20.007
+let c1 = "Hello"
+let d1 = true
+
+// const s = "This is a constant string". // s = "Trying to change the constant string" // This will throw an error
+
+console.log(a1 + b1)
+console.log(typeof a1)
+console.log(typeof c1)
+console.log(typeof d1)
+
+
---
+++ JsConcepts/loops/jsloops.js
@@ -0,0 +1,24 @@
+const flag = true;
+
+if (flag)
+    {
+    console.log("Flag is true");
+}
+else{
+    console.log("Flag is false");
+
+}
+
+// while loop
+
+let i = 0;
+while(i<10){
+    i++;
+    console.log(i);
+}
+
+// for loop
+
+for(let k=0; k<5; k++){
+    console.log(k);
+}
